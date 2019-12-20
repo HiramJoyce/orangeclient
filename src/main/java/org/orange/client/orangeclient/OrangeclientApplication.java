ed31790d6567441b8fcd4ebd0c298d2209c9a950
曹海亮 <1050588656@qq.com>
2019-12-20 17:10:08 +0800

    Initial commit

---
+++ src/main/java/org/orange/client/orangeclient/OrangeclientApplication.java
@@ -0,0 +1,14 @@
+package org.orange.client.orangeclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class OrangeclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrangeclientApplication.class, args);
+	}
+
+}
